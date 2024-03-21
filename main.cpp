1330ec21e2a37a4aa781302f24b4784e63bbd639
夏博焕 <9904365+xia-bohuan@user.noreply.gitee.com>
2024-03-21 13:35:25 +0800

    登录页面更新

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
-
-#include "mainwindow.h"
-
+#include "login.h"
+#include "appinit.h"
 #include <QApplication>
-
-
+#include "QFile"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QFile file("../database_crouse_design/settings.css");
+
+    if (file.open(QFile::ReadOnly)) {
+        QString qss = QLatin1String(file.readAll());
+        qApp->setStyleSheet(qss);
+        file.close();
+    }
+
+    QFont font("Arial", 10);
+    a.setFont(font);
+
+    AppInit::Instance()->start();
+
+
+    login loginpage;
+    loginpage.show();
     return a.exec();
 }
