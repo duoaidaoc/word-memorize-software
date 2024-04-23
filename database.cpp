e1c4863f44c6a057a7689c2ba9ae1f2ee0066b25
LiYangDou <109464584+micropuma@users.noreply.github.com>
2024-04-24 00:49:38 +0800

    add database

---
+++ database.cpp
@@ -0,0 +1,50 @@
+#include "database.h"
+
+// 构建数据库
+bool db::Database::createDatabase(const QString &dbName) {
+    db_ = QSqlDatabase::addDatabase("QSQLITE");
+    db_.setDatabaseName(dbName); // 使用传入的数据库名称参数
+    return db_.open();
+}
+
+// 查询数据库中是否有tableName表。
+bool db::Database::hasTable(const QString &tableName) {
+    QStringList tables = db_.tables();
+    if (tables.contains(tableName, Qt::CaseInsensitive)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//数据库的初始化。将几个必须用到的表格初始化
+bool db::Database::initDataBase() {
+    QStringList tables = db_.tables();
+    QSqlQuery query(db_);
+    // 构建学生表格
+    if (!tables.contains("students", Qt::CaseInsensitive) && !query.exec(createStudentTable)) {
+        qWarning() << "Failed to create student table:" << query.lastError().text();
+        return false;
+    }
+    // 构建老师表格
+    if (!tables.contains("teachers", Qt::CaseInsensitive) && !query.exec(createTeacherTable)) {
+        qWarning() << "Failed to create student table:" << query.lastError().text();
+        return false;
+    }
+    if (!tables.contains("wordbank", Qt::CaseInsensitive) && !query.exec(createWordBook)) {
+        qWarning() << "Failed to create word book:" << query.lastError().text();
+        return false;
+    }
+
+    if (!tables.contains("words", Qt::CaseInsensitive) && !query.exec(createWordTable)) {
+        qWarning() << "Failed to create word table:" << query.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
